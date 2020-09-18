13bcc84983b77d08c54c2ca6015f09da1ca863ef
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-18 02:44:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,5 +50,29 @@ def self.new_from_db(row)
   self.new(id, name, breed)
 end 
 
+def self.find_by_id(id)
+  sql = "SELECT * FROM students WHERE name = ?"
+  DB[:conn].execute(sql, id).collect { |row| new_from_db(row) }.first
+end
 
+def find_or_create_by(name:, breed:)
+  dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+  if !dog.empty?
+    dog_data = dog[0]
+    dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+  else 
+    dog = self.create(name: name, breed: breed)
+  end 
+  dog
+end 
+
+def update
+  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+  DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+  
+  def self.find_by_name(name)
+  sql = "SELECT * FROM students WHERE name = ?"
+  DB[:conn].execute(sql, name).collect { |row| new_from_db(row) }.first
+end
 end 
