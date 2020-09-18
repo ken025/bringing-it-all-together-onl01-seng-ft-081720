fe66ccb4e1445cb922b86ef7cf32e61ff4741034
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-18 02:54:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,7 +33,7 @@ def save
     
   DB[:conn].execute(sql, self.name, self.breed)
   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-  end
+  # end
 end
 
 def self.create(name, breed)
